449f1d3253c079ee78a209123742e223bebb0ed7
Roshini M V <roshiniroshini3504@gmail.com>
2026-05-18 16:24:57 +0530

    Completed

---
+++ settings.gradle.kts
@@ -0,0 +1,44 @@
+//pluginManagement {
+//    repositories {
+//        google {
+//            content {
+//                includeGroupByRegex("com\\.android.*")
+//                includeGroupByRegex("com\\.google.*")
+//                includeGroupByRegex("androidx.*")
+//            }
+//        }
+//        mavenCentral()
+//        gradlePluginPortal()
+//    }
+//}
+//plugins {
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+//}
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//}
+//
+//rootProject.name = "ShaleNammaPride"
+//include(":app")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ShaleNammaPride"
+
+include(":app")
